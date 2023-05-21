5bea5debbebbf4c365313e8e1b04a3e0865f01ea
Berecz Zsolt <bereczzsolt27@gmail.com>
2023-05-21 11:58:18 +0200

    Initial commit

---
+++ src/main/java/nye/progkor/beadando_film_katalogus/BeadandoFilmKatalogusApplication.java
@@ -0,0 +1,13 @@
+package nye.progkor.beadando_film_katalogus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeadandoFilmKatalogusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeadandoFilmKatalogusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nye/progkor/beadando_film_katalogus/BeadandoFilmKatalogusApplicationTests.java
@@ -0,0 +1,13 @@
+package nye.progkor.beadando_film_katalogus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadandoFilmKatalogusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
